763411121c8fed379bdade87d362be4015fa0b20
Adi Radu <retr00301@yahoo.com>
2021-06-30 11:47:44 +0300

    lectia 20

--- blockchain.js
+++ blockchain.js
@@ -29,6 +29,23 @@ class Blockchain
       }
       return true;
     }
+<<<<<<< HEAD
+=======
+
+    replaceChain(newChain) 
+    {
+      if (newChain.length <= this.chain.length) {
+        console.log('Received chain is not longer than the current chain.');
+        return;
+      } else if (!this.isValidChain(newChain)) {
+        console.log('The received chain is not valid.');
+        return;
+      }
+    
+      console.log('Replacing blockchain with the new chain.');
+      this.chain = newChain;
+    }
+>>>>>>> 7734906 (lectia 20)
 }
 
 module.exports = Blockchain;
